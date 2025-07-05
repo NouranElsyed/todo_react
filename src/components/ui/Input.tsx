13d8530e33ca648e59bb5b0d66f3120fca1c26ec
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2025-07-06 01:33:40 +0300

    enable input value tracking by properly forwarding ref and props

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,14 +1,16 @@
+import { forwardRef, type Ref } from "react"
+
 interface IProps {
     name: string
     type: string,
     className?: string,
     placeholder:string
 }
-const Input = ({name,type,className,placeholder}:IProps) => {
+const Input = forwardRef(({name,type,className,placeholder, ...props}:IProps , ref:Ref<HTMLInputElement>) => {
 const classes = `${className} w-full bg-zinc-700 rounded px-3 py-1 text-white placeholder:text-white`
   return (
-    <input name={name} type={type} className={classes} placeholder={placeholder}/>
+    <input ref={ref} name={name} type={type} className={classes} placeholder={placeholder} {...props}/>
   )
-}
+})
 
 export default Input
