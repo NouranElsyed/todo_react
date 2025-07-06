a4d128ebb1b5d8edc000f207ea38ae3db7c2078c
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2025-07-06 05:20:54 +0300

    add Axios setup to connect with API

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,23 +4,32 @@ import Input from "../components/ui/Input"
 import schema from "../utils/schema"
 import { yupResolver } from "@hookform/resolvers/yup";
 import ErrorMsg from "../components/ui/ErrorMsg";
+import api from "../config/axios.config";
 
 interface IFormInput {
-  userName: string
+  username: string
   email: string
   password: string
 }
 const Register = () => {
     const { register, handleSubmit ,formState: { errors }} = useForm<IFormInput>({resolver: yupResolver(schema)})
-    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+    const onSubmit: SubmitHandler<IFormInput> = async(data) => {
+      try{
+        console.log(data)
+        const response = await api.post("/auth/local/register",data)
+        console.log(response)
+      }catch(errors){
+        console.log(errors)
+      }
+    }
   return (
     <>
       <section className="w-full h-screen flex flex-col justify-center items-center ">
             <h3 className="text-2xl">Register to manage your todo 🤍</h3>
             <form onSubmit={handleSubmit(onSubmit)} className="w-1/4 flex flex-col justify-center items-center mt-10 space-y-5">
                 <div className="w-full flex flex-col ">
-                  <Input placeholder="User name..." type="text" {...register("userName")} />
-                  <ErrorMsg msg={errors?.userName?.message} display={true}/>
+                  <Input placeholder="User name..." type="text" {...register("username")} />
+                  <ErrorMsg msg={errors?.username?.message} display={true}/>
                 </div>
                 <div className="w-full flex flex-col ">
                 <Input placeholder="Email..." type="email" {...register("email")} />
--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,7 +1,7 @@
 import * as yup from "yup"
  const schema = yup
   .object({
-  userName: yup.string().min(3,"user name must be at least 3 characters").required("Username is required"),
+  username: yup.string().min(3,"user name must be at least 3 characters").required("Username is required"),
   email: yup.string().email("Invalid email").required("Email is required"),
   password: yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
   })
